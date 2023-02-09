378252ddcedcb36247b69475f76c69d1f2f8eec4
Subash-Company <Subashtech14@gmail.com>
2023-02-09 17:45:12 +0530

    09-02-2023

---
+++ src/main/java/abstractDemo/AbstractandNonabstract.java
@@ -0,0 +1,27 @@
+//package abstractDemo;
+// abstract class Vechicle{
+//	abstract void bike();  
+//    
+//    void car()  
+//    {  
+//        System.out.println("Car is running");  
+//    }  
+//}
+//class Honda extends Vechicle  
+//{  
+//   
+//    void bike() {  
+//        System.out.println("Bike is running");  
+//   
+//    }  
+//}
+//public class AbstractandNonabstract {
+//
+//	public static void main(String[] args) {
+//		Honda honda =new Honda();
+//		honda.bike();
+//		honda.car();
+//
+//	}
+//
+//}
---
+++ src/main/java/abstractDemo/BasicAbstract.java
@@ -0,0 +1,26 @@
+//package abstractDemo;
+//abstract class Vehicle  
+//{  
+//    abstract void bike();  
+//      
+//}  
+//class Honda extends Vehicle  
+//{  
+//  
+//     
+//    void bike() {  
+//        System.out.println("Bike is running");  
+//      
+//    }  
+//      
+//}  
+//
+//public class BasicAbstract {
+//
+//	public static void main(String[] args) {
+//		Honda honda =new Honda();
+//		honda.bike();
+//
+//	}
+//
+//}
