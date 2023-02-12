675cbe3bc0cb3f7163c4be301a884078ca4612dd
Subash <Subashtech14@gmail.com>
2023-02-12 23:10:04 +0530

    12-02-2023 Double Finished

---
+++ src/main/java/doubleDemo/DoubleDem.java
@@ -0,0 +1,12 @@
+package doubleDemo;
+
+public class DoubleDem {
+    public static void main(String[] args) {
+        double num1=581216732.323433;
+        double num2=7.83684987683688;
+        double num3=1.8732e3; //we can provide scientific notation
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+        System.out.println("num3: "+num3);
+    }
+}
